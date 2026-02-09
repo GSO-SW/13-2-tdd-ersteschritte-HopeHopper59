4a00c976c58719925cec0957e2c6910b62589c0d
Hope Hopper <162916189+HopeHopper59@users.noreply.github.com>
2026-02-09 11:04:09 +0100

    Add files via upload

--- 13.3_tdd_Geometrie/Rechteck.cs
+++ 13.3_tdd_Geometrie/Rechteck.cs
@@ -15,12 +15,38 @@ namespace _13._3_tdd_Geometrie
 
         public int Breite { 
             get { return breite; } 
-            set { breite = value; }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                breite = value; 
+            }
             
         }
         public int Hoehe { 
             get { return hoehe; } 
-            set { hoehe = value; }
+            set 
+            { 
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                hoehe = value; 
+            }
+        }
+
+        public Rechteck()
+        {
+            breite = 0;
+            hoehe = 0;
+        }
+
+        public Rechteck(int breite, int hoehe)
+        {
+            this.breite = breite;
+            this.hoehe = hoehe; 
         }
 
         public void Skalieren(double faktor)
@@ -31,6 +57,31 @@ namespace _13._3_tdd_Geometrie
             }
             breite = Convert.ToInt32(breite * faktor);
             hoehe = Convert.ToInt32(hoehe * faktor);
+        }
+
+        public int Umfang()
+        {
+            return (breite + hoehe) * 2;
+        }
+
+        public int Flaeche()
+        {
+            return breite * hoehe;
+        }
+
+        public bool GroeserAls(int breite, int hoehe)
+        {
+            int r1 = (this.breite + this.hoehe) * 2;
+            int r2 = (breite + hoehe) * 2;
+
+            if (r1 > r2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
